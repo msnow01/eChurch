d3028ab6ad1747366e7253d57f2d1bc038799ad3
msnow01 <miriamsnow98@gmail.com>
2021-04-22 13:45:19 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<?php $login_index_url = $base_url."account/login.php"; ?>
+<?php
+include "inc/header.php";
+$login_index_url = $base_url."account/login.php";
+?>
 <meta http-equiv="refresh" content="0; URL='<?php echo $login_index_url; ?>'" />
